32f61060555114e81ecd44ff03338350cdc22d34
Mathieu Benoit <mathben@technolibre.ca>
2026-02-17 14:38:21 -0500

    [UPD] nondominium-connector: force version 18

--- nondominium_connector/__manifest__.py
+++ nondominium_connector/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Nondominium Connector",
-    "version": "17.0.0.1.0",
+    "version": "18.0.0.1.0",
     "category": "Inventory",
     "summary": "Sync products to Nondominium (Holochain) via hc-http-gw",
     "description": """
@@ -13,7 +13,7 @@
         - Track sync status and Nondominium hashes per product
     """,
     "author": "Sensorica",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["product", "stock"],
     "data": [
         "security/ir.model.access.csv",
